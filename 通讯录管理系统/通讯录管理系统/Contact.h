5c85c364047e6ed4055a69c9c2726728adcfae84
amanda21088 <1663478999@qq.com>
2018-11-27 12:16:43 +0800

    结构体

---
+++ 通讯录管理系统/通讯录管理系统/Contact.h
@@ -0,0 +1,48 @@
+
+#ifndef __CONTACT_H__
+#define __CONTACT_H__
+#include<stdio.h>
+#include<assert.h>
+#include<string.h>
+enum Option
+{
+	EXIT,
+	ADD,
+	DEL,
+	SEARCH,
+	MODIFY,
+	SHOW,
+	EMPTY,
+	SORT
+};
+
+
+#define MAX 1000
+#define MAX_NAME 20
+#define SEX_MAX 5
+#define TELE_MAX 12
+#define ADDR_MAX 30
+typedef struct PeoInfo
+{
+	char name[MAX_NAME];
+	char sex[SEX_MAX];
+	short int age;
+	char tele[TELE_MAX];
+	char addr[ADDR_MAX];
+}PeoInfo;
+
+
+typedef struct Contact
+{
+	PeoInfo data[MAX]; //存放信息
+	int sz;//通讯录有效信息的个数
+}Contact;
+
+void InitContact(Contact* pcon);
+void AddContact(Contact* pcon);
+void ShowContact(const Contact* pcon);
+void DelContact(Contact* pcon);
+void SearchContact(const Contact*pcon);
+void ModifyContact(Contact*pcon);
+void EmptyContact(Contact *pcon);
+#endif //_CONTACT_H__
