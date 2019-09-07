24ccd34d478e6cd5cff121836644c59f2cb47958
amanda21088 <1663478999@qq.com>
2019-09-07 16:18:33 +0800

    每日一题

---
+++ 模拟用户登录情景，并且只能登录三次。（只允许输入三次密码，如果密码正确则提示 登录成，如果三次均输入错误，则退出程序/模拟用户登录情景，并且只能登录三次。（只允许输入三次密码，如果密码正确则提示 登录成，如果三次均输入错误，则退出程序.cpp
@@ -0,0 +1,31 @@
+
+
+#define _CRT_SECURE_NO_WARNINGS 1
+
+}
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+	char psw[10] = "";
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < 3; i++)
+	{
+		printf("please input:");
+		scanf("%s", &psw);
+		if (strcmp(psw, "password") == 0)//条件成立
+			break;
+	}
+	if (i == 3)
+	{
+		printf("exit\n");
+	}
+	else
+	{
+		printf("log in\n");
+	}
+	system("pause");
+	return 0;
+}
