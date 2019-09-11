91f7a51076f336fe5416937fef0deab45125f0aa
amanda21088 <1663478999@qq.com>
2019-09-11 16:56:24 +0800

    判断机器的大小端

---
+++ 判断机器大小端/判断机器的大小端.cpp
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+// 代码1
+int check_sys()
+{
+	int i = 1;
+	return (*(char*)&i);
+}
+int main()
+{
+	int ret = check_sys();
+	if (1 == ret)
+	{
+		printf("小端\n");
+	}
+	else
+	{
+		printf("大端\n");
+	}
+	system("pause");
+	return 0;
+}
+
+//代码2
+int check_sys()
+{
+	union
+	{
+		int a;
+		char b;
+	}un;
+	un.a = 1;
+	return un.b;
+}
