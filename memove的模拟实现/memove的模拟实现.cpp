d4f96a44c4a84781079741387accfe12671f2fa3
amanda21088 <1663478999@qq.com>
2019-09-19 15:11:06 +0800

    memove的模拟实现.cpp

---
+++ memove的模拟实现/memove的模拟实现.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+void* memmove(void * dst, const void* src, size_t count)
+{
+	void * ret = dst;
+	if (dst <= src || (char *)dst >= ((char *)src + count))
+	{
+		while (count--)
+		{
+			*(char*)dst = *(char*)src;
+			dst = (char*)dst + 1;
+			src = (char*)src + 1;
+		}
+	}
+	else
+	{
+		dst = (char*)dst + count - 1;
+		src = (char*)src + count - 1;
+		while (count--)
+		{
+			*(char*)dst = *(char*)src;
+			dst = (char*)dst - 1;
+			src = (char*)src - 1;
+		}
+	}
+	return ret;
+}
+
